2ac43df5f0a078a5350852f94b1361a405e65b3d
RFigueroa <48028483+FRobertoFigueroaC@users.noreply.github.com>
2024-04-17 20:49:34 -0600

    Route view

--- routes/web.php
+++ routes/web.php
@@ -6,14 +6,9 @@ use App\Models\Job;
 
 
 
-Route::get('/', function () {
-    $jobs = Job::all();
-
-    return view('home');
-});
-
+Route::view('/', 'home');
+Route::view('/contact', 'contact');
 // Index
-// Route::resource('/jobs', [JobController::class, 'index']);
 Route::get('/jobs', [JobController::class, 'index']);
 // Create
 Route::get('/jobs/create', [JobController::class, 'create']);
@@ -27,12 +22,3 @@ Route::get('/jobs/{job}/edit', [JobController::class, 'edit']);
 Route::patch('/jobs/{job}', [JobController::class, 'update']);
 // Destroy
 Route::delete('/jobs/{job}', [JobController::class, 'destroy']);
-
-
-
-
-
-
-Route::get('/contact', function () {
-    return view('contact');
-});

d9adb48241db6cee129baa16e0c08782299913ca
RFigueroa <48028483+FRobertoFigueroaC@users.noreply.github.com>
2024-04-17 20:54:28 -0600

    route resource

--- routes/web.php
+++ routes/web.php
@@ -2,26 +2,27 @@
 
 use App\Http\Controllers\JobController;
 use Illuminate\Support\Facades\Route;
-use App\Models\Job;
 
 
 
 Route::view('/', 'home');
 Route::view('/contact', 'contact');
 // Resource
-Route::controller(JobController::class, 'index')->group(function (){
-  // Index
-  Route::get('/jobs', 'index');
-  // Create
-  Route::get('/jobs/create', 'create');
-  // Show
-  Route::get('/jobs/{job}', 'show');
-  // Store
-  Route::post('/jobs', 'store');
-  // Edit
-  Route::get('/jobs/{job}/edit', 'edit');
-  // Update
-  Route::patch('/jobs/{job}', 'update');
-  // Destroy
-  Route::delete('/jobs/{job}', 'destroy');
-});
+// Route::controller(JobController::class, 'index')->group(function (){
+//   // Index
+//   Route::get('/jobs', 'index');
+//   // Create
+//   Route::get('/jobs/create', 'create');
+//   // Show
+//   Route::get('/jobs/{job}', 'show');
+//   // Store
+//   Route::post('/jobs', 'store');
+//   // Edit
+//   Route::get('/jobs/{job}/edit', 'edit');
+//   // Update
+//   Route::patch('/jobs/{job}', 'update');
+//   // Destroy
+//   Route::delete('/jobs/{job}', 'destroy');
+// });
+
+Route::resource('jobs', JobController::class);

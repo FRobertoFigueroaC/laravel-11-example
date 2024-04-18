4bfdae7a31d3665011454672da4f210cc6c4f5cb
RFigueroa <48028483+FRobertoFigueroaC@users.noreply.github.com>
2024-04-17 20:56:23 -0600

    ending of section 3

--- routes/web.php
+++ routes/web.php
@@ -8,21 +8,4 @@ use Illuminate\Support\Facades\Route;
 Route::view('/', 'home');
 Route::view('/contact', 'contact');
 // Resource
-// Route::controller(JobController::class, 'index')->group(function (){
-//   // Index
-//   Route::get('/jobs', 'index');
-//   // Create
-//   Route::get('/jobs/create', 'create');
-//   // Show
-//   Route::get('/jobs/{job}', 'show');
-//   // Store
-//   Route::post('/jobs', 'store');
-//   // Edit
-//   Route::get('/jobs/{job}/edit', 'edit');
-//   // Update
-//   Route::patch('/jobs/{job}', 'update');
-//   // Destroy
-//   Route::delete('/jobs/{job}', 'destroy');
-// });
-
 Route::resource('jobs', JobController::class);

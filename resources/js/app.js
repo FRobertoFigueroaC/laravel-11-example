b47fa544ee3eefca9e12c51420119c86c64a75bb
RFigueroa <48028483+FRobertoFigueroaC@users.noreply.github.com>
2024-05-12 15:15:51 -0600

    vite configs

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,4 +1,5 @@
 import './bootstrap';
+import '../css/app.css';
 
 
 console.log('Hello from js file');
--- resources/views/Components/layout.blade.php
+++ resources/views/Components/layout.blade.php
@@ -8,7 +8,7 @@
   {{-- <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" --}}
     {{-- integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous"> --}}
   <title>30 Days of Laravel</title>
-  @vite(['resources/css/app.css', 'resources/js/app.js'])
+  @vite(['resources/js/app.js'])
 </head>
 <body class="h-full">
 <div class="min-h-full">
